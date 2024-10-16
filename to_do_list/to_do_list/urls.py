c9c2130f575ee961594ac23c72fef109b025caa1
Haro Mikhaylov <www.haris@bk.ru>
2024-10-16 16:47:52 +0300

    alter routes

--- to_do_list/to_do_list/urls.py
+++ to_do_list/to_do_list/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
+from api.views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('auth/', include('rest_framework.urls')),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
