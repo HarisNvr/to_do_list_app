b5178baaa32b26a41141f14cd0e6f45d31acda57
Haro Mikhaylov <www.haris@bk.ru>
2024-10-16 12:37:29 +0300

    add apps.py and register the tasks app

--- to_do_list/to_do_list/settings.py
+++ to_do_list/to_do_list/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
