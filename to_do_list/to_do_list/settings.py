55a7e104676b7b7cb30e5e04c711f90132cfa879
Haro Mikhaylov <www.haris@bk.ru>
2024-10-16 15:15:02 +0300

    add 'api' app to settings.py

--- to_do_list/to_do_list/settings.py
+++ to_do_list/to_do_list/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'tasks',
     'rest_framework',
     'rest_framework_simplejwt'
