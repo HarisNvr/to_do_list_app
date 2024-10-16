6b422e933f9f296dba6118fb9496398708630d55
Haro Mikhaylov <www.haris@bk.ru>
2024-10-16 12:33:34 +0300

    add models.py

---
+++ to_do_list/tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from to_do_list.tasks.constants import TITLE_LENGTH
+
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=TITLE_LENGTH)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='todos'
+    )
+
+    def __str__(self):
+        return self.title
